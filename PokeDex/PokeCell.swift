32f205c21457f163de792ef1b42dd4cacbcd75b5
Edouard Barbier <EdouardBarbier@edouards-MacBook-Pro.local>
2016-01-08 21:16:16 +0000

    Done UI for UICollectionViewCell

---
+++ PokeDex/PokeCell.swift
@@ -0,0 +1,18 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by Edouard Barbier on 08/01/16.
+//  Copyright © 2016 Edouard Barbier. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    
+    
+}
---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Edouard Barbier on 08/01/16.
+//  Copyright © 2016 Edouard Barbier. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }    
+}
