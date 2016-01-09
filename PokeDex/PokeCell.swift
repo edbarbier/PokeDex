5949a09ea8335c03e1e272793435d7b46a409ddd
Edouard Barbier <EdouardBarbier@edouards-MacBook-Pro.local>
2016-01-09 10:19:12 +0000

    added music + custom fonts

--- PokeDex/PokeCell.swift
+++ PokeDex/PokeCell.swift
@@ -13,6 +13,19 @@ class PokeCell: UICollectionViewCell {
     @IBOutlet weak var thumbImg: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
     
+    var pokemon: Pokemon!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 10.0
+    }
     
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
     
 }
